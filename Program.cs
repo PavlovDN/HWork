aba5f375976313c10399e2e62a1e12790cc21889
PavlovDN <114232908+PavlovDN@users.noreply.github.com>
2022-11-18 00:55:04 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,30 @@
 ﻿/*
-Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-m = 2, n = 3 -> A(m,n) = 9
-m = 3, n = 2 -> A(m,n) = 29
-*/
-
-int ResultNumbers(int m, int n)
-{
-    if (m == 0)
-        return n + 1;
-    if (n == 0)
-        return ResultNumbers (m - 1, 1);
-    else
-        return ResultNumbers(m - 1, ResultNumbers(m, n - 1)); ;
-}
-
-Console.Clear();
-Console.WriteLine("Введите 1-е число: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите 2-е число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-while (m < 0 || n < 0)
-{
-    Console.WriteLine();
-    Console.WriteLine(" Число должно быть положительным или 0 ");
-    Console.WriteLine();
+    Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+    M = 1; N = 15 -> 120
+    M = 4; N = 8. -> 30
+    */
+    
+    int ResultNumbers(int m, int n)
+    {
+        if (m == n)
+            return m;
+        return ResultNumbers(m + 1, n) + m;
+    }
+    
+    Console.Clear();
     Console.WriteLine("Введите 1-е число: ");
-    m = Convert.ToInt32(Console.ReadLine());
+    int m = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Введите 2-е число: ");
-    n = Convert.ToInt32(Console.ReadLine());
-}
-    Console.WriteLine($"Функция Аккермана равна A(m,n): {ResultNumbers(m, n)}");
+    int n = Convert.ToInt32(Console.ReadLine());
+    while (m >= n)
+    {
+        Console.WriteLine();
+        Console.WriteLine("1-е число должно быть меньше второго");
+        Console.WriteLine();
+        Console.WriteLine("Введите 1-е число: ");
+        m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите 2-е число: ");
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine($"Сумма всех чисел в диапазоне от {m} до {n} равна: {ResultNumbers(m, n)}");
+
